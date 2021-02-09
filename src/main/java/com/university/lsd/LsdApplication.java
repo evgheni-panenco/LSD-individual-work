19e350af10979b3ac7e617234f067bcea17b2878
Evgheni Panenco <evgheni.panenco@gmail.com>
2021-02-09 15:55:19 +0200

    Create project structure

---
+++ src/main/java/com/university/lsd/LsdApplication.java
@@ -0,0 +1,13 @@
+package com.university.lsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsdApplication.class, args);
+	}
+
+}
